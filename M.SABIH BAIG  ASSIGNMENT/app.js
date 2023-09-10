bc417149c3d1ebd13a15a0696a3b83bbe80cff6e
SabihBaig <143556056+SabihBaig@users.noreply.github.com>
2023-09-10 19:10:29 +0500

    Add files via upload

---
+++ M.SABIH BAIG  ASSIGNMENT/app.js
@@ -0,0 +1,34 @@
+// CHAPTER 01
+// A1
+var firstname = 'Muhammad'
+alert(firstname);
+// A2
+var lastname = 'Sabih Baig'
+alert(lastname);
+// A3
+var Emailid = 'S.h@mtex.com'
+alert(Emailid);
+// A4
+var PhoneNumber = '0900-23456'
+alert(PhoneNumber);
+// A5
+var Password = '2wderx431'
+alert(Password);
+// A6
+var statement = 'You are  learning JAVASCRIPT'
+alert(statement);
+// A7
+var msg= 'Friends Javascript is wonderful language'
+alert(msg)
+
+// CHAPTER 02
+
+var FirstName
+
+var state = (hi);
+
+var state = 'Hi guys how are you all' 
+alert(state);
+
+var teamNName = 'lions tagers'
+alert(teamName);
